4a1b9764ae0ff7c81abdcc28766f1504dd590f27
vivek <vivekananda462@gmail.com>
2026-08-11 11:52:38 +0530

    Day20 - util classe added

---
+++ Dat-20/util/AuthController.java
@@ -0,0 +1,47 @@
+package com.training.demo_train_service.controller;
+
+import com.training.demo_train_service.dto.LoginRequest;
+import com.training.demo_train_service.dto.LoginResponse;
+import com.training.demo_train_service.security.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthController(
+            AuthenticationManager authenticationManager,
+            JwtService jwtService) {
+
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest loginRequest) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        String token = jwtService.generateToken(
+                loginRequest.getUsername()
+        );
+
+        return ResponseEntity
+                .<LoginResponse>ok(new LoginResponse(token));
+    }
+}
